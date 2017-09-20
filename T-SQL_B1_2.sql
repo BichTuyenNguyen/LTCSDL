ad35f9621e06aaa6801b7f6cc0308b23c5cc74d3
BichTuyenNguyen <bichtuyen6897@gmail.com>
2017-09-20 08:56:26 +0700

    thuc hanh buoi 1_2

---
+++ T-SQL_B1_2.sql
@@ -0,0 +1,17 @@
+﻿-- CÂU 11: TÍNH TỔNG 2 SỐ
+IF OBJECT_ID('uspCau1','P') IS NOT NULL
+DROP PROC uspCau1
+GO	
+
+CREATE PROC uspCau1
+	@a float = 0.0,
+	@b float = 0.0,
+	@tong float = 0.0 out
+AS
+	 SET @tong = @a + @b
+GO
+
+-- 
+DECLARE @tong float = 0.0
+EXEC   uspCau1 1.1 , 2.2 , @tong OUT
+PRINT @tong
